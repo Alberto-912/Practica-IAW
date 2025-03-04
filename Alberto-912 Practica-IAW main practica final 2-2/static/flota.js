73b68879c0e2ada986b4498d0654eb6dd1c158b5
Alberto-912 <eliteyyzesp@gmail.com>
2025-03-04 12:14:51 +0100

    Delete Alberto-912 Practica-IAW main practica final 2-2 directory

--- Alberto-912 Practica-IAW main practica final 2-2/static/flota.js
+++
@@ -1,4 +0,0 @@
-// Coge la información del coche para pasarlo a la pagina del alquiler.html
-function seleccionarCoche(id, name, price) {
-    window.location.href = `/alquiler?id=${id}&name=${encodeURIComponent(name)}&price=${encodeURIComponent(price)}`;
-}
